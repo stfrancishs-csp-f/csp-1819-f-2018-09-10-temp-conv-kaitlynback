a7e4836c37c29a1f3a302ff1b540ca099e7a22be
Kaitlyn Back <20back@stfrancishs.org>
2018-09-07 19:50:53 +0000

    init

---
+++ fahrenheitcelsius.c
@@ -0,0 +1,14 @@
+/* fahrenheitcelsius.c
+*/
+
+#include <stdio.h>
+int main(void)
+
+{
+int fahr;
+
+for (fahr = 0; fahr <= 300; fahr = fahr +20)
+{
+    printf( "f - %3d c - %6.lf\n", fahr, (5.0/9.0)*(fahr-32));
+}
+}
