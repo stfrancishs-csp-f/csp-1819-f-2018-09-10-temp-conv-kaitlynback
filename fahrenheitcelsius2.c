b4dcc05123abaacc5dc333332324d2ae72e1eecf
Kaitlyn Back <20back@stfrancishs.org>
2018-09-11 17:20:23 +0000

    edit while playing around with program

--- fahrenheitcelsius2.c
+++ fahrenheitcelsius2.c
@@ -6,10 +6,12 @@ int main(void)
 
 {
 int fahr;
+double answer;
 
 for (fahr = -50; fahr <= 120; fahr = fahr +5)
 {
-    printf( "f - %3d c - %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    answer =(5.0/9.0)*(fahr-32);
+    printf( "f - %3d c - %6.1f\n", fahr, answer);
 }
 return 0;
 }
